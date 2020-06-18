f66742dadda7a1171218b225f0057fbd1d357612
WnndGws <WnndGws@users.noreply.github.com>
2020-06-18 20:00:43 +0800

    Start work on network module

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 5000;
+const unsigned int interval = 1000;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
@@ -63,5 +63,5 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ ram_perc, "M%s%%", },
+	{ netspeed_rx, "W%s", "wlan0"},
 };
